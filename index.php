4e8aeb7b15102bf0f92afcc9d15faaf4bfa07dd2
Lasse Bredahl Vangsgaard <lass9782@edu.campusvejle.dk>
2017-06-15 10:00:45 +0200

    Wrapper og menu

--- index.php
+++ index.php
@@ -4,9 +4,21 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="CSS/style.css">
-    <title>Document</title>
+    <title>Wilderness Blog</title>
 </head>
 
-<body> </body>
+<body>
+    <div class="wrapper">
+        <header> <img src="IMG/header_img.jpg" alt="logo">
+            <nav>
+                <ul>
+                    <li> <a href="index.php">HOME</a> </li>
+                    <li> <a href="">Planter</a></li>
+                    <li> <a href="">Indretning</a> </li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+</body>
 
 </html>
