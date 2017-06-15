75e675646564df0e78e839a04e6016f9015edecf
Lasse Bredahl Vangsgaard <lass9782@edu.campusvejle.dk>
2017-06-15 13:02:15 +0200

    Artikel og aside

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Wilderness Blog</title>
 </head>
 
@@ -12,12 +13,32 @@
         <header> <img src="IMG/header_img.jpg" alt="logo">
             <nav>
                 <ul>
-                    <li> <a href="index.php">HOME</a> </li>
+                    <div class="gray">
+                        <li> <a href="index.php">HOME</a> </li>
+                    </div>
                     <li> <a href="">Planter</a></li>
                     <li> <a href="">Indretning</a> </li>
                 </ul>
+                <ul>
+                    <li><a href="">Log ind</a></li>
+                </ul>
             </nav>
         </header>
+        <main>
+            <aside><img src="IMG/frk_overspringshandling.png" alt="Line">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi iusto laboriosam, repellat vitae dolor in. Dolorem sint consequuntur eaque, modi numquam possimus? Placeat optio debitis enim explicabo ab nihil. Magnam.</p> <i class="fa fa-instagram fa-2x" aria-hidden="true"></i> <i class="fa fa-facebook-square fa-2x" aria-hidden="true"></i> <i class="fa fa-flickr fa-2x" aria-hidden="true"></i> <i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i> </aside>
+            <div class="post">
+                <article>
+                    <h1>Vores nye lænestol i egetræ og kerneleder</h1> <img src="IMG/l%C3%A6nestol.jpg" alt="lænestol">
+                    <div class="afsender">
+                        <p>Skrevet af</p> Line
+                        <p>Den</p>
+                    </div>
+                    <div class="time">24. August, 2017</div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam autem fugiat iste eius perferendis omnis voluptate rerum cum cupiditate. Nisi quia saepe ipsa, reiciendis similique odio itaque, non officiis tempore!</p>
+                </article>
+            </div>
+        </main>
     </div>
 </body>
 
